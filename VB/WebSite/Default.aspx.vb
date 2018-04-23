6970a4bbce9b34f37f643660fa1b6022919ab148
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,36 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load()
+		If (Not IsPostBack) AndAlso (Not IsCallback) Then
+			hField("categoryId") = Nothing
+		End If
+	End Sub
+	Protected Sub grid_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
+		'This emulates long-time data binding		
+		System.Threading.Thread.Sleep(3000)
+	End Sub
+	Protected Sub panel1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+		gridCategories.Visible = True
+		gridCategories.DataBind()
+	End Sub
+	Protected Sub panel2_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+		gridProducts.Visible = True
+	End Sub
+	Protected Sub gridProducts_CustomCallback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewCustomCallbackEventArgs)
+		gridProducts.DataBind()
+	End Sub
+	Protected Sub grid2_DataBinding(ByVal sender As Object, ByVal e As EventArgs)
+		'This emulates long-time data binding. Data binding occurs only if the 'categoryId' value is set.
+		If hField("categoryId") IsNot Nothing Then
+			System.Threading.Thread.Sleep(3000)
+		End If
+	End Sub
+	Protected Sub grid_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim g As ASPxGridView = TryCast(sender, ASPxGridView)
+		g.Visible = g.IsCallback
+	End Sub
+End Class
